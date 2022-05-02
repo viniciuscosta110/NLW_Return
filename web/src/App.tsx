dd391c27f1ab299864234634e9e551546acb74cc
viniciuscosta110 <viniciuscosta1900@hotmail.com>
2022-05-02 11:40:17 -0300

    "Set environment"

---
+++ web/src/App.tsx
@@ -0,0 +1,20 @@
+
+interface ButtonProps {
+  text?: string
+}
+
+function Button(props : ButtonProps) {	
+  return <button className="bg-violet-600 px-4 h-10 rounded text-violet-100 hover:bg-violet-900 transition-colors">{props.text ?? 'Default'}</button>
+}
+
+function App() {
+  return (
+    <div className="flex gap-2">
+      <Button text="Enviar" />
+      <Button text="Ok" />
+      <Button />
+    </div>
+  )
+}
+
+export default App
