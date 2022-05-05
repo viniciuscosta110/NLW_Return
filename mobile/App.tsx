b0f8d7360bcb0fed757bebb52df2282cb9ed3ac1
viniciuscosta110 <viniciuscosta1900@hotmail.com>
2022-05-05 11:27:17 -0300

    Mobile setup

---
+++ mobile/App.tsx
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import { View } from 'react-native';
+
+import { theme } from './src/theme';
+import { Widget } from './src/components/Widget';
+
+
+export default function App() {
+  return (
+    <View style={{
+      flex: 1,
+      backgroundColor: theme.colors.background,
+    }}>
+      <Widget />
+      <StatusBar style='light' backgroundColor='transparent' translucent />
+    </View>
+  );
+}
+
